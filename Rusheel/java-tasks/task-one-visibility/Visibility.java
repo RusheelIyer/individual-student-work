068f3fd760bae995240e922b030df429a1c94645
RusheelIyer <rusheel2911@gmail.com>
2015-01-28 20:07:02 +0400

    Update Visibility.java

--- Rusheel/java-tasks/task-one-visibility/Visibility.java
+++ Rusheel/java-tasks/task-one-visibility/Visibility.java
@@ -1,10 +1,58 @@
 class PublicPerson {
+    String foreName;
+	String surName;
+	int birthYear;
+	String name;
+	int age;
+	
+	public PublicPerson(String foreName, String surName, int birthYear){
+		this.name = foreName + surName;
+		this.age = 2015 - birthYear;
+	}
+	
+	public String getForeName(){
+		return foreName;
+	}
+	
+	public String getSurName(){
+		return surName;
+	}
+	
+	public int getBirthYear(){
+		return birthYear;
+	}
+    
 }
 
 class PrivatePerson {
+    String foreName;
+	String surName;
+	int birthYear;
+	String name;
+	
+	public PrivatePerson(String foreName, String surName, int birthYear) {
+		this.name = foreName + surName;
+		this.birthYear = birthYear;
+	}
+
+	public int getAge() {
+		return 2015 - birthYear;
+	}
+	
+	public String getName(){
+		return name;
+		}
 }
 
 public class Visibility {
+    
+    private static String whoIsOlder(PrivatePerson personOne, PublicPerson personTwo) {
+		if(personOne.getAge() > personTwo.age){
+			return personOne.getName();
+		} else{
+			return personTwo.name;
+		}
+	}
     public static void main(String[] args) {
         PrivatePerson personOne = new PrivatePerson("James", "Smith", 2005);
         System.out.println("Created a new person " + personOne.getName()
